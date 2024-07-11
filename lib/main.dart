f0bb4368487ca8859ae4dfadb88fedd79c6e1acf
Hagwan Mohammed <79087273+Hagwan@users.noreply.github.com>
2024-07-12 01:03:48 +0300

    Update theme and SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:lottie/lottie.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<Uint8List> fetchImage(String inputText) async {
-  final apiKey = 'YOUR_API_KEY';
-  final url =
+  const apiKey = 'YOUR_API_KEY';
+  const url =
       'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0';
 
   final response = await http.post(
@@ -19,12 +24,7 @@ Future<Uint8List> fetchImage(String inputText) async {
     }),
   );
 
-  // Print response for debugging
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-
   if (response.statusCode == 200) {
-    // The response should contain binary image data directly
     return response.bodyBytes;
   } else {
     print('Failed to load image: ${response.body}');
@@ -36,20 +36,58 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: _themeMode,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        textTheme: GoogleFonts.lilitaOneTextTheme(),
       ),
-      home: MyHomePage(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        textTheme:
+            GoogleFonts.lilitaOneTextTheme().apply(bodyColor: Colors.white),
+      ),
+      home: FlutterSplashScreen(
+        useImmersiveMode: true,
+        duration: const Duration(milliseconds: 7000),
+        nextScreen: MyHomePage(toggleTheme: _toggleTheme),
+        backgroundColor: Color.fromARGB(255, 57, 57, 57),
+        splashScreenBody: Center(
+          child: Lottie.asset(
+            "assets/images/Texttoimage.json",
+            repeat: false,
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final VoidCallback toggleTheme;
+
+  const MyHomePage({super.key, required this.toggleTheme});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -62,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _generateImage() async {
     setState(() {
       _isLoading = true;
-      _imageData = null; // Clear previous image
+      _imageData = null;
     });
     try {
       final imageData = await fetchImage(_controller.text);
@@ -83,16 +121,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+                isDarkMode ? Icons.wb_sunny_rounded : Icons.nightlight_round),
+            onPressed: widget.toggleTheme,
+          ),
+        ],
         centerTitle: true,
-        title: const Text('Generate Image from Text'),
+        title: const Text('TextArt'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.center, // Stretch for full width
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(height: 20),
             _isLoading
@@ -102,23 +148,22 @@ class _MyHomePageState extends State<MyHomePage> {
                         ? Image.memory(
                             _imageData!,
                             width: double.infinity,
-                            fit: BoxFit.cover, // Cover the entire space
+                            fit: BoxFit.cover,
                           )
                         : const Center(
                             child: Text(
                               'Start image generating by entering text and clicking on generate',
-                              style: TextStyle(fontSize: 16),
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
                             ),
                           ),
                   ),
             const SizedBox(height: 20),
             Row(
-              // Use Row to position elements horizontally
-      
-              mainAxisAlignment: MainAxisAlignment.end, // Space evenly
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Expanded(
-                  // Stretch the text field
                   child: TextField(
                     controller: _controller,
                     decoration: const InputDecoration(
@@ -128,7 +173,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 IconButton(
-                    onPressed: _generateImage, icon: const Icon(Icons.send))
+                  onPressed: _generateImage,
+                  icon: const Icon(Icons.send),
+                ),
               ],
             ),
           ],
